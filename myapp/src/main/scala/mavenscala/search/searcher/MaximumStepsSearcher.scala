b0bddd8a101a7647521eefe4e54a4a703ffefa2e
wjingyao2008 <317705784@qq.com>
2016-05-04 15:49:04 +0800

    refactor the maximumSteps

--- myapp/src/main/scala/mavenscala/search/searcher/MaximumStepsSearcher.scala
+++ myapp/src/main/scala/mavenscala/search/searcher/MaximumStepsSearcher.scala
@@ -9,27 +9,28 @@ import mavenscala.search.RouteIterator
 class MaximumStepsSearcher(val startName: String,
                            val stopName: String,
                            maxStops: Int) extends Searcher {
-  val maxRouteNbr=maxStops+1
+  val maxRouteNbr = maxStops + 1
 
   def isSameInMaximumSteps(route: Seq[Edge]): Boolean = {
-    val size=route.size
-     return (size<=maxRouteNbr && size>1 &&
-       route.last.toStation.name==stopName)
+    val size = route.size
+    val isSameWithLast = route.last.toStation.name == stopName
+    return (size <= maxRouteNbr && size > 1 &&
+      isSameWithLast)
   }
 
   override def search(iterator: RouteIterator): String = {
-    var numberOfRoute=0
+    var numberOfRoute = 0
     while (iterator.hasNext) {
       val route = iterator.next
 
-      if(route.head.toStation.name!=startName){
-        val name=route.head.toStation.name
+      if (route.head.toStation.name != startName) {
+        val name = route.head.toStation.name
         iterator.dropCurrentStation
       }
-      else if(isSameInMaximumSteps(route)){
-        numberOfRoute+=1
+      else if (isSameInMaximumSteps(route)) {
+        numberOfRoute += 1
       }
-      else if(route.length > maxRouteNbr){
+      else if (route.length > maxRouteNbr) {
         iterator.dropCurrentStation
       }
 
